5fc963fe165c051750e6cb335e991b047ac8bb5a
shubham-patidar-roostify <spatidar@roostify.com>
2019-07-24 15:03:35 +0530

    Initial commit

---
+++ src/main/java/com/loanknit/frodo/FrodoApplication.java
@@ -0,0 +1,13 @@
+package com.loanknit.frodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrodoApplication.class, args);
+	}
+
+}
